f0c3d81f7fb315040220d54669297c04ec7508f7
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-27 21:58:48 +0000

    feat: registered models in site admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Vehicle, VehicleImages
 
 # Register your models here.
+
+class VehicleImagesInlineAdmin(admin.TabularInline):
+    model = VehicleImages
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    inlines = [VehicleImagesInlineAdmin]
+    prepopulated_fields = {'slug': ('make', 'model', 'trim', 'fuel', 'price')}
+
+    class Meta:
+        model = Vehicle
+
+@admin.register(VehicleImages)
+class VehicleImagesAdmin(admin.ModelAdmin):
+    pass
