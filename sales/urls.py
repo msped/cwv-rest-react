54d269383ea9e406cc9f3d9d3d29c8c412fe0a91
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-29 15:49:53 +0000

    feat: added vehicle detail uri

--- sales/urls.py
+++ sales/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListVehicles
+from .views import ListVehicles, VehicleDetail
 
 urlpatterns = [
-    path('', ListVehicles.as_view(), name="list_vehicles"),
+    path('', ListVehicles.as_view(), name=""),
+    path('/<int:vehicle_id>', VehicleDetail.as_view(), name="vehicle_detail")
 ]
