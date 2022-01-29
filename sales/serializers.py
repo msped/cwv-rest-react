e386f43d9f246c3e74f273966810bcb810da4715
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-29 14:40:12 +0000

    added vehicle and vehicleiamges serializers

---
+++ sales/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Vehicle, VehicleImages
+
+class VehicleImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleImages
+        fields = [
+            'id',
+            'vehicle',
+            'image',
+            'is_main'
+        ]
+
+class VehicleSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, obj):
+        images = VehicleImages.objects.filter(vehicle_id=obj.id)
+        serializer = VehicleImagesSerializer(images, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Vehicle
+        fields = [
+            'id',
+            'slug',
+            'make',
+            'model',
+            'trim',
+            'year',
+            'fuel',
+            'body_type',
+            'car_state',
+            'reserved',
+            'mileage',
+            'engine_size',
+            'mot_expiry',
+            'extras',
+            'price',
+            'images'
+        ]
