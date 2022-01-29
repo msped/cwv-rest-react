23e56df7bba7a2deb06a38f1f5cf501807412a3e
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-29 14:45:32 +0000

    feat: added ListVehicles view

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Vehicle
+from .serializers import VehicleSerializer
 
 # Create your views here.
+
+class ListVehicles(ListAPIView):
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
