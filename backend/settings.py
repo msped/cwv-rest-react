e2ec9eee472b9a5d2a6d43857fef68d5acc71428
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-29 22:00:41 +0000

    feat: setup up email settings in django

--- backend/settings.py
+++ backend/settings.py
@@ -148,3 +148,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
