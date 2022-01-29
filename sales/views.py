a48896e6fc1b74e90ded5f860ee4ac05bdba5a0c
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-29 15:49:43 +0000

    feat: added vehicledetail view

--- sales/views.py
+++ sales/views.py
@@ -7,3 +7,10 @@ from .serializers import VehicleSerializer
 class ListVehicles(ListAPIView):
     serializer_class = VehicleSerializer
     queryset = Vehicle.objects.all()
+
+class VehicleDetail(RetrieveAPIView):
+    serializer_class = VehicleSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'vehicle_id'
+    queryset = Vehicle.objects.all()
+    
