ba548deb49142f10ac70265783a1b0f206aa5f46
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-30 16:45:12 +0000

    refactor: added trailing slashes to uris

--- backend/urls.py
+++ backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/sales', include('sales.urls'), name="sales"),
+    path('api/sales/', include('sales.urls'), name="sales"),
 ]
