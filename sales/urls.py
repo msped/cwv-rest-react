489a4aa0133abe4f6692fb333b18c71202fc12fa
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-30 01:33:42 +0000

    feat: added reserve uri removed all appending slashes

--- sales/urls.py
+++ sales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListVehicles, VehicleDetail, VehicleState
+from .views import ListVehicles, VehicleDetail, VehicleState, SendReservation
 
 urlpatterns = [
     path('', ListVehicles.as_view(), name="vehicle_list"),
-    path('/<int:vehicle_id>', VehicleDetail.as_view(), name="vehicle_detail"),
-    path('/state/<int:vehicle_id>', VehicleState.as_view(), name="vehicle_state"),
+    path('<int:vehicle_id>', VehicleDetail.as_view(), name="vehicle_detail"),
+    path('state/<int:vehicle_id>', VehicleState.as_view(), name="vehicle_state"),
+    path('reserve/<int:vehicle_id>', SendReservation.as_view(), name="send_reservation"),
 ]
