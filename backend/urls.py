eb0a4aaa5f42c2c05664a6ab25a829d85f8256ff
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 01:15:50 +0000

    feat: added gallery uri to main project

--- backend/urls.py
+++ backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sales/', include('sales.urls'), name="sales"),
+    path('api/gallery/', include('gallery.urls'), name="gallery"),
 ]
