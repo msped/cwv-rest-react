6c47a247416e6c6c41c023c06236351fbe646efe
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 14:08:00 +0000

    feat: regsiter model admins

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import GalleryImage, GalleryItem
 
 # Register your models here.
+
+class GalleryImagesInlineAdmin(admin.TabularInline):
+    model = GalleryImage
+
+@admin.register(GalleryItem)
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [GalleryImagesInlineAdmin]
+    prepopulated_fields = {'slug': ('make', 'model', 'trim', 'power')}
+
+    class Meta:
+        model = GalleryItem
+
+@admin.register(GalleryImage)
+class GalleryImagesAdmin(admin.ModelAdmin):
+    pass
