2c8e61f8ea85f4a4e3ede3eca9d954da488a6295
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 20:26:47 +0000

    feat: added business_admin urls to project urls

--- backend/urls.py
+++ backend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/sales/', include('sales.urls'), name="sales"),
     path('api/gallery/', include('gallery.urls'), name="gallery"),
     path('api/contact/', include('contact.urls'), name="contact"),
+    path('api/admin/', include('business_admin.urls'), name="business_admin"),
 ]
