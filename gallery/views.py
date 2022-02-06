53b97cb31cb1b44e21d0b1a3a5227b60c1cf5873
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 14:11:00 +0000

    feat: added list gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from gallery.serializers import GalleryItemSerializer
+from .models import GalleryItem
 
 # Create your views here.
+
+class Gallery(ListAPIView):
+    serializer_class = GalleryItemSerializer
+    queryset = GalleryItem.objects.all()
