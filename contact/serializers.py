a8231fa75cd37e6220b9e14a4d0a623b6c88a5b4
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 18:26:44 +0000

    fix: added disable to for pylint W0223 in serializer

--- contact/serializers.py
+++ contact/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from phonenumber_field.serializerfields import PhoneNumberField
 
+#pylint: disable=W0223
+
 class ContactSerializer(serializers.Serializer):
     name = serializers.CharField()
     phone_number = PhoneNumberField()
