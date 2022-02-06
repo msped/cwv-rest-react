d0120f1dae010f292f753c8a176baf2a0bdb26b5
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 14:17:07 +0000

    feat: added gallerydetail view

--- gallery/views.py
+++ gallery/views.py
@@ -8,3 +8,9 @@ from .models import GalleryItem
 class Gallery(ListAPIView):
     serializer_class = GalleryItemSerializer
     queryset = GalleryItem.objects.all()
+
+class GalleryDetail(RetrieveAPIView):
+    serializer_class = GalleryItemSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "gallery_id"
+    queryset = GalleryItem.objects.all()
