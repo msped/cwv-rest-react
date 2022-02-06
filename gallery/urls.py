952ef0c21b971fe038b088de53448fccd505e186
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-06 14:16:51 +0000

    feat: added gallery detail url

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Gallery
+from .views import Gallery, GalleryDetail
 
 urlpatterns = [
     path('', Gallery.as_view(), name="gallery_view"),
+    path('<int:gallery_id>/', GalleryDetail.as_view(), name="gallery_detail_view"),
 ]
