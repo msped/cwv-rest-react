acce36f458982ad231a4158871b6e366436628ad
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-27 23:54:47 +0000

    feat: added invoicegeneration view

--- business_admin/views.py
+++ business_admin/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .utils import invoice_handler
 
 # Create your views here.
+
+class InvoiceGeneration(APIView):
+    def post(self, request, *args, **kwargs):
+        email = invoice_handler(request.data)
+        return Response(
+            {f"Invoice sent by email to {email}."},
+            status=status.HTTP_200_OK
+        )
