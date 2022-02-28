898b27d7e35d63a883689cedb2bba73ff6f34963
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-28 21:49:31 +0000

    feat: added auth app to project urls

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('authentication.urls'), name="auth"),
     path('api/sales/', include('sales.urls'), name="sales"),
     path('api/gallery/', include('gallery.urls'), name="gallery"),
     path('api/contact/', include('contact.urls'), name="contact"),
