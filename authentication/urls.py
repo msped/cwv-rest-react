f0c7f1b8e27203017cfd0fa52ac1efcde2385a26
Matt Edwards <mattsp.edwards@googlemail.com>
2022-02-28 23:56:07 +0000

    feat: added url for blacklisttokenview

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
+from .views import BlacklistTokenView
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/blacklist', BlacklistTokenView.as_view(), name="auth_logout")
 ]
