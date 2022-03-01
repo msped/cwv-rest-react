7365a4eae4d18fdd42e5700057515ab7f5b904b0
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-01 00:40:43 +0000

    style: append slash to app urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from .views import BlacklistTokenView
 
 urlpatterns = [
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/blacklist', BlacklistTokenView.as_view(), name="auth_logout")
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/blacklist/', BlacklistTokenView.as_view(), name="auth_logout")
 ]
