bdc26194f6e74e24f0e3628012836dc2178dbae8
Matt Edwards <mattsp.edwards@googlemail.com>
2022-03-01 00:40:52 +0000

    test: testing tokens for jwt

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestAuthentication(APITestCase):
+
+    def setUp(self):
+        get_user_model().objects.create_user(
+            username="test",
+            email="matt@mspe.me",
+            password="5up3R!98"
+        )
+
+    def test_request_access_token(self):
+        response = self.client.post(
+            '/api/auth/token',
+            {
+                'username': 'test',
+                'email': 'matt@mspe.me',
+                'password': '5up3R!98'
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_request_refresh_token(self):
+        access_request = self.client.post(
+            '/api/auth/token',
+            {
+                'username': 'test',
+                'email': 'matt@mspe.me',
+                'password': '5up3R!98'
+            },
+            format='json'
+        )
+        refresh_token = access_request.data['refresh']
+        response = self.client.post(
+            '/api/auth/token/refresh',
+            {
+                'refresh': refresh_token
+            },
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
