985a6a3341837ac984439fb2a3c3bdb128c536f0
Matt Edwards <mattsp.edwards@googlemail.com>
2022-04-18 17:51:14 +0100

    feat: added reserve button

---
+++ frontend/src/components/Reserve.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    Button,
+} from '@mui/material'
+
+export default function Reserve() {
+  return (
+    <React.Fragment>
+        <Button variant='contained'>
+            Reserve Vehicle
+        </Button>
+    </React.Fragment>
+  )
+}
