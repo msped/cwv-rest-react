42c6e23239a8f53082b3046bbffffbd22216d7f7
Matt Edwards <mattsp.edwards@googlemail.com>
2022-04-25 23:27:22 +0100

    refactor: added gallerylist item to only get main image

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -11,6 +11,28 @@ class GalleryImagesSerializer(serializers.ModelSerializer):
             "is_main"
         ]
 
+class GalleryItemListSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, obj):
+        images = GalleryImage.objects.get(item_id=obj.id, is_main=True)
+        serializer = GalleryImagesSerializer(images, many=False)
+        return serializer.data
+
+    class Meta:
+        model = GalleryItem
+        fields = [
+            "id",
+            "slug",
+            "make",
+            "model",
+            "trim",
+            "year",
+            "power",
+            "description",
+            "images"
+        ]
+
 class GalleryItemSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
 
